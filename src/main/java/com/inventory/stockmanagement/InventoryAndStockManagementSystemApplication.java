02f9a91df78101c672eb51ef7482561ae0d7b373
Lashan Achinthaka <165979953+LashAchinthaka@users.noreply.github.com>
2026-05-20 00:49:28 +0530

    Initial commit: Inventory and Stock Management System

---
+++ src/main/java/com/inventory/stockmanagement/InventoryAndStockManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.inventory.stockmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryAndStockManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryAndStockManagementSystemApplication.class, args);
+    }
+
+}
